f2b49ebc3892cb2bf951dce089fc1753d1ac3ad4
Ashenafi Endashaw <asfyd42@gmail.com>
2022-11-28 11:27:44 -0300

    Swiched from webpack to vite

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Home from "./Component/home/index" 
+import { Routes, Route } from 'react-router-dom';
+import Teachers from './Component/Teacher/all-teachers';
+import Teacherdetail from './Component/Teacher/teacher-detail';
+import Addteacher from './Component/Teacher/add-teacher';
+
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Routes>
+        <Route path='/teacher' element={<Teachers />}></Route>
+        <Route path='/detail' element={<Teacherdetail />}></Route>
+        <Route path='/Add/teacher' element={<Addteacher />}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
